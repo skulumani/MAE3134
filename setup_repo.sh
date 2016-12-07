965445527a8bd872f7103d867d11f0ac479b8549
Shankar Kulumani <shanks.k@gmail.com>
2016-12-07 18:55:47 -0500

    Modify repo name

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # add the correct remote repositories
-git_repo="git@github.com:skulumani/MAE3134_Spring2017_syllabus.git"
-bb_repo="git@bitbucket.org:shankarkulumani/MAE3134_Spring2017_syllabus.git"
-fdcl_repo="git@github.com:fdcl-gwu/MAE3134_Spring2017_syllabus.git"
+git_repo="git@github.com:skulumani/MAE3134_Spring2017.git"
+bb_repo="git@bitbucket.org:shankarkulumani/MAE3134_Spring2017.git"
+fdcl_repo="git@github.com:fdcl-gwu/MAE3134_Spring2017.git"
 
 printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
 printf "Github: $git_repo\n"
