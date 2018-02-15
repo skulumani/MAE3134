1f295839904c6a97047261c4b31d13f2d638405c
Shankar Kulumani <shanks.k@gmail.com>
2018-02-15 12:01:22 -0500

    Update prob5 python

--- homework/HW3/prob5.py
+++ homework/HW3/prob5.py
@@ -15,3 +15,11 @@ time, resp = signal.step(sys)
 fig, ax = plt.subplots()
 ax.plot(time, resp)
 plt.show()
+
+# find poles for a specifc response
+Ts = 8
+Tp = 4
+OS = 0.2
+
+# calculate damping ratio and natural frequency from this
+zeta = 1
