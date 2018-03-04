fdc9d652c4bc60aaf6f2943ec14c5cafd12377df
Shankar Kulumani <shanks.k@gmail.com>
2018-03-04 14:43:35 -0500

    Update solutions

--- exams/midterm/prob1.py
+++ exams/midterm/prob1.py
@@ -8,10 +8,15 @@ Ts2 = 10
 zeta = - np.log(OS) / np.sqrt(np.pi**2 + np.log(OS)**2)
 theta = np.arccos(zeta)*180/np.pi
 wd = np.pi / Tp
+sigma1 = 4 / Ts1
+sigma2 = 4 / Ts2
 wn1 = 4 / zeta / Ts1
 wn2 = 4 / zeta / Ts2
 
 print("Zeta > %3.2f" % zeta)
-print("Theta > %3.2f" % theta)
-print("Wd > %3.2f" % wd)
-print("Wn > %3.2f" % wn)
+print("Theta < %3.2f" % theta)
+print("{} < sigmad < {}".format(sigma2, sigma1))
+print("{} < wn < {}".format(sigma2/zeta, sigma1/zeta))
+print("wd > {}".format(wd))
+
+
