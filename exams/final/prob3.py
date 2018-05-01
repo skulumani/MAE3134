237e5bd91f0b45b98352ba7794369fca3aeb1244
Shankar Kulumani <shanks.k@gmail.com>
2018-04-30 20:48:07 -0400

    add untracked files

---
+++ exams/final/prob3.py
@@ -0,0 +1,18 @@
+"""Bode plot problem
+"""
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+gain = 500
+zeros = np.array([-100, -20])
+roots = np.roots([1, 8, 25])
+poles = np.array([0, roots[0], roots[1]])
+
+w, mag, phase = signal.bode((zeros, poles, gain), np.logspace(-1, 3))
+
+plt.figure()
+plt.semilogx(w, mag)
+plt.figure()
+plt.semilogx(w, phase)
+plt.show()
