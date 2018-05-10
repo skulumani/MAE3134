e195ebfe53f18ce04a6e539620a1d2f3032a2290
Shankar Kulumani <shanks.k@gmail.com>
2018-05-10 09:10:01 -0400

    Update final exam

--- exams/final/prob3.py
+++ exams/final/prob3.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-gain = 500
+gain = 1/20
 zeros = np.array([-100, -20])
 roots = np.roots([1, 8, 25])
 poles = np.array([0, roots[0], roots[1]])
